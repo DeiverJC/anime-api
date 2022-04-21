3c9822bd9946a67a2a7c36fe076be2c80f53cbd5
DeiverJC <deiver-jc@live.com>
2022-04-20 21:24:09 -0500

    Initial commit

---
+++ index.js
@@ -0,0 +1,32 @@
+require("dotenv").config();
+const express = require("express");
+const mongoose = require("mongoose");
+
+// Setup server
+const app = express();
+const port = process.env.PORT || 3000;
+
+// Setup mongodb
+mongoose.connect(process.env.MONGO_URI, {
+  useNewUrlParser: true,
+  useUnifiedTopology: true,
+});
+const { Schema } = mongoose;
+
+// Model
+const animeSchema = Schema({
+  title: { type: String, required: true },
+  synopsis: { type: String },
+  episodeCount: { type: Number, required: true },
+  startDate: { type: Date },
+});
+
+const Anime = mongoose.model("Anime", animeSchema);
+
+// Routes
+app.get("/", (req, res) => {
+  res.json({ message: "Hello World" });
+});
+
+// Start server
+app.listen(port, () => console.log(`Server on port ${port}`));
